412718ef80d7ee46c2e9fee32145da1638417574
Vladimir <vladimirarevshatyan@gmail.com>
2016-08-30 14:49:35 +0400

    added color utils for tingint and etc for future use

---
+++ app/src/main/java/ink/utils/ColorUtils.java
@@ -0,0 +1,21 @@
+package ink.utils;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+
+/**
+ * Created by PC-Comp on 8/30/2016.
+ */
+public class ColorUtils {
+    public static Drawable tintDrawable(Context context, int drawableResource, int desiredColor) {
+        Drawable circleDrawable = ContextCompat.getDrawable(context, drawableResource);
+        Drawable wrappedDrawable = DrawableCompat.wrap(circleDrawable);
+        if (circleDrawable != null && wrappedDrawable != null) {
+            circleDrawable.mutate();
+            DrawableCompat.setTint(wrappedDrawable, desiredColor);
+        }
+        return wrappedDrawable;
+    }
+}
