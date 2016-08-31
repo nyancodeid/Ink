dcfa839227f61898542b34060c7db1b8d1146c8b
Vladimir <vladimirarevshatyan@gmail.com>
2016-08-31 16:06:50 +0400

    added result callbacks

--- app/src/main/java/ink/activities/BaseActivity.java
+++ app/src/main/java/ink/activities/BaseActivity.java
@@ -1,11 +1,22 @@
 package ink.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Toast;
 
+import com.ink.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import ink.interfaces.AccountDeleteListener;
 import ink.utils.SharedHelper;
 
@@ -18,15 +29,41 @@ public abstract class BaseActivity extends AppCompatActivity {
     private SharedHelper sharedHelper;
     private View decorView;
 
+    @Nullable
+    @Bind(R.id.customizeToolbar)
+    Toolbar customizeToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.gc();
         sharedHelper = new SharedHelper(this);
         decorView = getWindow().getDecorView();
 
+        ButterKnife.bind(decorView);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setBackgroundDrawable(new ColorDrawable(Color.YELLOW));
+        }
+        Toolbar customizeToolbar = (Toolbar) decorView.findViewById(R.id.customizeToolbar);
+        if (customizeToolbar != null) {
+            Toast.makeText(BaseActivity.this, "not null", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(BaseActivity.this, "null", Toast.LENGTH_SHORT).show();
+        }
     }
 
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        Toolbar customizeToolbar = (Toolbar) parent.findViewById(R.id.customizeToolbar);
+        if (customizeToolbar != null) {
+            Toast.makeText(BaseActivity.this, "not null", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(BaseActivity.this, "null", Toast.LENGTH_SHORT).show();
+        }
+        return super.onCreateView(parent, name, context, attrs);
+    }
 
     protected void setOnAccountDeleteListener(AccountDeleteListener accountDeleteListener) {
         this.accountDeleteListener = accountDeleteListener;
@@ -49,7 +86,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d("fasfasffsafasfas", "onActivityResult: "+"on activity called in base");
         super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/ink/activities/CustomizeLook.java
+++ app/src/main/java/ink/activities/CustomizeLook.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,7 +28,7 @@ import yuku.ambilwarna.AmbilWarnaDialog;
  * Created by USER on 2016-08-30.
  */
 
-public class CustomizeLook extends AppCompatActivity {
+public class CustomizeLook extends BaseActivity {
 
     @Bind(R.id.customizeToolbar)
     Toolbar customizeToolbar;
