99549b4c60db98555157fbac262a721eaaf17e20
vladimirarevshatyan <vladimirarevshatyan@gmail.com>
2016-09-13 02:09:15 +0400

    added some good changes to month and name

--- app/src/main/java/ink/activities/Chat.java
+++ app/src/main/java/ink/activities/Chat.java
@@ -345,7 +345,7 @@ public class Chat extends BaseActivity implements RecyclerItemClickListener, Pro
                     String weekName = new SimpleDateFormat("EEEE").format(calendar.getTime());
 
                     builder.setMessage(getString(R.string.dateOfMessage) + calendar.get(Calendar.YEAR) + "," + weekName + "," + monthName
-                            + " " + (calendar.get(Calendar.DAY_OF_MONTH) < 10 ? "0" + calendar.get(Calendar.DAY_OF_MONTH) : calendar.get(Calendar.DAY_OF_MONTH))
+                            + "-" + (calendar.get(Calendar.DAY_OF_MONTH) < 10 ? "0" + calendar.get(Calendar.DAY_OF_MONTH) : calendar.get(Calendar.DAY_OF_MONTH))
                             + " (" + (calendar.get(Calendar.HOUR_OF_DAY) < 10 ? "0" + calendar.get(Calendar.HOUR_OF_DAY) : calendar.get(Calendar.HOUR_OF_DAY)) + ":"
                             + (calendar.get(Calendar.MINUTE) < 10 ? "0" + calendar.get(Calendar.MINUTE) : calendar.get(Calendar.MINUTE)) +
                             ":" + (calendar.get(Calendar.SECOND) < 10 ? "0" + calendar.get(Calendar.SECOND) : calendar.get(Calendar.SECOND)) + ")");
@@ -369,7 +369,7 @@ public class Chat extends BaseActivity implements RecyclerItemClickListener, Pro
                     String weekName = new SimpleDateFormat("EEEE").format(calendar.getTime());
 
                     builder.setMessage(getString(R.string.dateOfMessage) + calendar.get(Calendar.YEAR) + "," + weekName + "," + monthName
-                            + " " + (calendar.get(Calendar.DAY_OF_MONTH) < 10 ? "0" + calendar.get(Calendar.DAY_OF_MONTH) : calendar.get(Calendar.DAY_OF_MONTH))
+                            + "-" + (calendar.get(Calendar.DAY_OF_MONTH) < 10 ? "0" + calendar.get(Calendar.DAY_OF_MONTH) : calendar.get(Calendar.DAY_OF_MONTH))
                             + " (" + (calendar.get(Calendar.HOUR_OF_DAY) < 10 ? "0" + calendar.get(Calendar.HOUR_OF_DAY) : calendar.get(Calendar.HOUR_OF_DAY)) + ":"
                             + (calendar.get(Calendar.MINUTE) < 10 ? "0" + calendar.get(Calendar.MINUTE) : calendar.get(Calendar.MINUTE)) +
                             ":" + (calendar.get(Calendar.SECOND) < 10 ? "0" + calendar.get(Calendar.SECOND) : calendar.get(Calendar.SECOND)) + ")");
