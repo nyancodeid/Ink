074874dbb5a328db622ef9b5c822f94e070acad6
Vladimir <vladimirarevshatyan@gmail.com>
2016-09-13 12:47:42 +0400

    couple fixes

--- app/src/main/java/ink/adapters/TrendAdapter.java
+++ app/src/main/java/ink/adapters/TrendAdapter.java
@@ -3,15 +3,19 @@ package ink.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ink.R;
+import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
@@ -37,7 +41,7 @@ public class TrendAdapter extends RecyclerView.Adapter<TrendAdapter.ViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(TrendAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(final TrendAdapter.ViewHolder holder, int position) {
         final TrendModel trendModel = trendModelArrayList.get(position);
         if (trendModel.isTop()) {
             holder.premiumBadgeIcon.setVisibility(View.VISIBLE);
@@ -46,12 +50,27 @@ public class TrendAdapter extends RecyclerView.Adapter<TrendAdapter.ViewHolder> 
             holder.premiumBadgeIcon.setVisibility(View.INVISIBLE);
             holder.premiumText.setVisibility(View.GONE);
         }
-        holder.trendTitle.setText(trendModel.getTitle());
-        holder.trendContent.setText(trendModel.getContent());
-        if (trendModel.getImageUrl() != null && !trendModel.getImageUrl().isEmpty()) {
-            holder.trendImage.setVisibility(View.VISIBLE);
-            Ion.with(context).load(trendModel.getImageUrl()).withBitmap().placeholder(R.drawable.whats_trending_vector).intoImageView(holder.trendImage);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            holder.trendTitle.setText(Html.fromHtml(trendModel.getTitle(), Html.FROM_HTML_MODE_LEGACY));
+            holder.trendContent.setText(Html.fromHtml(trendModel.getContent(), Html.FROM_HTML_MODE_LEGACY));
         } else {
+            holder.trendTitle.setText(Html.fromHtml(trendModel.getTitle()));
+            holder.trendContent.setText(Html.fromHtml(trendModel.getContent()));
+        }
+
+
+        if (trendModel.getImageUrl() != null && !trendModel.getImageUrl().isEmpty()) {
+            holder.imageLoadingProgress.setVisibility(View.VISIBLE);
+            holder.trendImage.setVisibility(View.VISIBLE);
+            Ion.with(context).load(trendModel.getImageUrl()).withBitmap().placeholder(R.drawable.whats_trending_vector).intoImageView(holder.trendImage).setCallback(new FutureCallback<ImageView>() {
+                @Override
+                public void onCompleted(Exception e, ImageView result) {
+                    holder.imageLoadingProgress.setVisibility(View.GONE);
+                }
+            });
+        } else {
+            holder.imageLoadingProgress.setVisibility(View.GONE);
             holder.trendImage.setVisibility(View.GONE);
         }
         holder.trendViewMore.setOnClickListener(new View.OnClickListener() {
@@ -78,6 +97,7 @@ public class TrendAdapter extends RecyclerView.Adapter<TrendAdapter.ViewHolder> 
         private ImageView trendImage;
         private ImageView premiumBadgeIcon;
         private Button trendViewMore;
+        private ProgressBar imageLoadingProgress;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -86,6 +106,7 @@ public class TrendAdapter extends RecyclerView.Adapter<TrendAdapter.ViewHolder> 
             premiumText = (TextView) itemView.findViewById(R.id.premiumText);
             trendImage = (ImageView) itemView.findViewById(R.id.trendImage);
             premiumBadgeIcon = (ImageView) itemView.findViewById(R.id.premiumBadgeIcon);
+            imageLoadingProgress = (ProgressBar) itemView.findViewById(R.id.imageLoadingProgress);
             trendViewMore = (Button) itemView.findViewById(R.id.trendViewMore);
         }
     }
