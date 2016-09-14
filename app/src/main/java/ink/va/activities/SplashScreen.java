efda5ed0ec0d1758913e9e4452e2779867eb4051
Vladimir <vladimirarevshatyan@gmail.com>
2016-09-14 12:16:12 +0400

    added key hash check

--- app/src/main/java/ink/va/activities/SplashScreen.java
+++ app/src/main/java/ink/va/activities/SplashScreen.java
@@ -1,18 +1,50 @@
 package ink.va.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
 
 import com.ink.va.R;
 
 public class SplashScreen extends BaseActivity {
+    private boolean isAppOriginal;
+    private String firstKeyHash = "GXEMUTFFeZejMCClv1bXr7Zbid8=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Intent intent = new Intent(this, Intro.class);
-        startActivity(intent);
-        finish();
+        String currentKeyHash = ink.va.utils.Debug.getKeyHash(this);
+
+        if (currentKeyHash.trim().equals(firstKeyHash.trim())) {
+            isAppOriginal = true;
+        }
+        if (isAppOriginal) {
+            Intent intent = new Intent(this, Intro.class);
+            startActivity(intent);
+            finish();
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getString(R.string.thisWontWork));
+            builder.setMessage(getString(R.string.applicationModified));
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        }
+
     }
 }
