ed3033a16c44550988200f203485f94ddd7b467f
vladimirarevshatyan <vladimirarevshatyan@gmail.com>
2016-11-17 23:46:18 +0400

    added first steps for who viewed profile logic

--- app/src/main/java/ink/va/utils/Constants.java
+++ app/src/main/java/ink/va/utils/Constants.java
@@ -5,7 +5,7 @@ package ink.va.utils;
  */
 public class Constants {
     public static final String LINKEDIN_HOST = "api.linkedin.com";
-    public static final String PEOPLE_LINKEDIN_URL = "https://" + LINKEDIN_HOST + "/v1/people/~:(first-name,last-name,public-profile-url,picture-url)";
+    public static final String PEOPLE_LINKEDIN_URL = "https://" + LINKEDIN_HOST + "/v1/people/~:(first-name,last-name,public-profile-url)";
     public static final String LINKEDIN_SHARE_URL = "https://" + LINKEDIN_HOST + "/v1/people/~/shares";
     public static final String MAIN_URL = "http://104.196.118.124/Ink_Base_project_1992_no_15_dp_deployed_57_olAllolclslf_8954/";
     public static final String USER_IMAGES_FOLDER = "UserImages/";
