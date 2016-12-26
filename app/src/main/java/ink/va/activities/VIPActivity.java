bbf213dbe7bed571f9161259cc7d2f7321644069
Vladimir <vladimirarevshatyan@gmail.com>
2016-12-26 19:04:07 +0400

    minor improvements with vip chat

--- app/src/main/java/ink/va/activities/VIPActivity.java
+++ app/src/main/java/ink/va/activities/VIPActivity.java
@@ -3,20 +3,32 @@ package ink.va.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.ink.va.R;
+import com.koushikdutta.ion.Ion;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ink.va.utils.Constants;
 
 public class VIPActivity extends BaseActivity {
+
+    @Bind(R.id.rootImageView)
+    ImageView rootImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vip);
+        ButterKnife.bind(this);
         setStatusBarColor(R.color.vip_status_bar_color);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        Ion.with(this).load(Constants.ANDROID_DRAWABLE_DIR + "vip_background").intoImageView(rootImageView);
     }
 
 
