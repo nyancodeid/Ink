0a5b11409b3de8d9d3786c8db43f723704b41b59
Vladimir <vladimirarevshatyan@gmail.com>
2017-01-10 14:08:33 +0400

    added global vip chat activity

---
+++ app/src/main/java/ink/va/activities/GlobalVipChat.java
@@ -0,0 +1,14 @@
+package ink.va.activities;
+
+import android.os.Bundle;
+
+import com.ink.va.R;
+
+public class GlobalVipChat extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_vip_chat);
+    }
+}
