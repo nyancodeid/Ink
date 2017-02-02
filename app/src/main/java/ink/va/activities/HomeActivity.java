dea532e34013f31991f94dc5b441617b11574b8f
Vladimir <vladimirarevshatyan@gmail.com>
2017-02-02 19:50:20 +0400

    added thread to init the pollfish

--- app/src/main/java/ink/va/activities/HomeActivity.java
+++ app/src/main/java/ink/va/activities/HomeActivity.java
@@ -63,7 +63,6 @@ import java.io.IOException;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import fab.FloatingActionButton;
-import ink.va.callbacks.GeneralCallback;
 import ink.va.fragments.Feed;
 import ink.va.fragments.MyFriends;
 import ink.va.interfaces.AccountDeleteListener;
@@ -844,17 +843,8 @@ public class HomeActivity extends BaseActivity
 
     private void initPollFish() {
         DimDialog.showDimDialog(this, getString(R.string.loadingSurvey));
-        pollFish.initPollFish(new GeneralCallback() {
-            @Override
-            public void onSuccess(Object o) {
-                pollFish.hidePollFish();
-            }
-
-            @Override
-            public void onFailure(Object o) {
-
-            }
-        });
+        pollFish.initPollFish();
+        pollFish.hidePollFish();
 
     }
 
