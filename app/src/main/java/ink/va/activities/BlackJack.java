550ee6229426dcdcd43f9c69ae65e55befdcc4d8
vladimirarevshatyan <vladimirarevshatyan@gmail.com>
2017-02-03 00:43:06 +0400

    added assets for the game of black jack and generally in any card game

---
+++ app/src/main/java/ink/va/activities/BlackJack.java
@@ -0,0 +1,14 @@
+package ink.va.activities;
+
+import android.os.Bundle;
+
+import com.ink.va.R;
+
+public class BlackJack extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_black_jack);
+    }
+}
