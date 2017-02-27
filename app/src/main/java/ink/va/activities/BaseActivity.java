1e655107e2fc341de695f0f5806061249a0a0060
Vladimir <vladimirarevshatyan@gmail.com>
2017-02-27 17:19:44 +0400

    fixed crash

--- app/src/main/java/ink/va/activities/BaseActivity.java
+++ app/src/main/java/ink/va/activities/BaseActivity.java
@@ -20,7 +20,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -454,7 +453,6 @@ public abstract class BaseActivity extends AppCompatActivity {
         }
         if (messageService != null) {
             messageService.emit(EVENT_PING, pingJson);
-            Log.d("MessageService", "ping: pinging");
         }
     }
 
