f3c7f7fb4493c8cd303082e87a9367e91a6cc773
Vladimir <vladimirarevshatyan@gmail.com>
2017-02-28 15:33:48 +0400

    minor fixes

--- app/src/main/java/ink/va/activities/MafiaRoomActivity.java
+++ app/src/main/java/ink/va/activities/MafiaRoomActivity.java
@@ -1,18 +1,34 @@
 package ink.va.activities;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
 
 import com.ink.va.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import ink.va.utils.SharedHelper;
 
 public class MafiaRoomActivity extends BaseActivity {
 
+    @BindView(R.id.roomRecycler)
+    RecyclerView roomRecycler;
+    private SharedHelper sharedHelper;
+    @BindView(R.id.addRoomButton)
+    FloatingActionButton addRoomButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mafia_room);
         ButterKnife.bind(this);
+        sharedHelper = new SharedHelper(this);
+        if (sharedHelper.getMenuButtonColor() != null) {
+            addRoomButton.setColorFilter(Color.parseColor(sharedHelper.getMenuButtonColor()),
+                    PorterDuff.Mode.SRC_ATOP);
+        }
     }
 }
