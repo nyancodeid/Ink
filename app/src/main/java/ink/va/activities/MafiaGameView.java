54d6a08b6928f9aabe0da331b82f1d6588dc05ef
vladimirarevshatyan <vladimirarevshatyan@gmail.com>
2017-03-02 00:48:44 +0400

    added game view

---
+++ app/src/main/java/ink/va/activities/MafiaGameView.java
@@ -0,0 +1,14 @@
+package ink.va.activities;
+
+import android.os.Bundle;
+
+import com.ink.va.R;
+
+public class MafiaGameView extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mafia_game_view);
+    }
+}
