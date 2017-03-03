5f0572da681a4e2c6b97952e729011806588a1be
Vladimir <vladimirarevshatyan@gmail.com>
2017-03-03 17:23:52 +0400

    added animations

--- app/src/main/java/ink/va/activities/MafiaGameView.java
+++ app/src/main/java/ink/va/activities/MafiaGameView.java
@@ -5,6 +5,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -42,12 +45,21 @@ public class MafiaGameView extends BaseActivity {
     TextView mafiaRoleExplanationTV;
     @BindView(R.id.mafiaRoleHolder)
     ImageView mafiaRoleHolder;
+    @BindView(R.id.closeRoleView)
+    Button closeRoleView;
+    private Animation slideUpAniation;
+    private Animation slideDownAnimation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mafia_game_view);
         ButterKnife.bind(this);
+
+        slideUpAniation = AnimationUtils.loadAnimation(this, R.anim.slide_up_slow);
+        slideDownAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_down_slow);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             mafiaRoomsModel = Parcels.unwrap(extras.getParcelable("mafiaRoomsModel"));
@@ -65,15 +77,49 @@ public class MafiaGameView extends BaseActivity {
 
     @OnClick(R.id.replyToRoomIV)
     public void replyToRoomIVClicked() {
-        mafiaRoleView.setVisibility(View.VISIBLE);
+        openRoleView(R.drawable.mafia_sniper_role);
     }
 
-    @OnClick(R.id.nightDayIV)
-    public void sss() {
-        mafiaRoleHolder.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.mafia_sniper_role));
+    @OnClick(R.id.closeRoleView)
+    public void closeRoleClicked() {
+        closeRoleView();
     }
 
     private void clearImage() {
         mafiaRoleHolder.setImageDrawable(null);
+    }
+
+    private void setButtonState(boolean enabled) {
+        closeRoleView.setEnabled(enabled);
+    }
+
+    private void openRoleView(int roleResourceId) {
+        mafiaRoleHolder.setImageDrawable(ContextCompat.getDrawable(this, roleResourceId));
+        mafiaRoleView.setVisibility(View.VISIBLE);
+        setButtonState(true);
+        mafiaRoleView.startAnimation(slideDownAnimation);
+    }
+
+    private void closeRoleView() {
+        clearImage();
+        mafiaRoleHolder.setImageDrawable(null);
+        setButtonState(false);
+        slideUpAniation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mafiaRoleView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mafiaRoleView.startAnimation(slideUpAniation);
     }
 }
