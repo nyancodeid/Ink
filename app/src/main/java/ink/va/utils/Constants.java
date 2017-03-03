9a756f0eaf163103eb69106ff6420d9bc4812b03
Vladimir <vladimirarevshatyan@gmail.com>
2017-03-03 19:56:59 +0400

    added options menus

--- app/src/main/java/ink/va/utils/Constants.java
+++ app/src/main/java/ink/va/utils/Constants.java
@@ -61,6 +61,7 @@ public class Constants {
     public static final String NOTIFY_OPPONENT = "NotifyOpponent.php";
     public static final String DELETE_MAFIA_ROOM = "DeleteMafiaRoom.php";
     public static final String LEAVE_MAFIA_ROOM = "LeaveMafiaRoom.php";
+    public static final String MAFIA_ROOM_PARTICIPANTS = "GetMafiaRoomParticipants.php";
     public static final String JOIN_MAFIA_ROOM = "JoinMafiaRoom.php";
     public static final String RESPOND_TO_REQUEST_URL = "RespondToRequest.php";
     public static final String REMOVE_FRIEND_URL = "RemoveFriend.php";
