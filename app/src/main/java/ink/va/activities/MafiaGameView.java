e90e039ef629730fac03ceff1e4f1ad1f24187b2
Vladimir <vladimirarevshatyan@gmail.com>
2017-03-03 13:19:01 +0400

    added icons

--- app/src/main/java/ink/va/activities/MafiaGameView.java
+++ app/src/main/java/ink/va/activities/MafiaGameView.java
@@ -1,23 +1,46 @@
 package ink.va.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 import com.ink.va.R;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ink.va.models.MafiaRoomsModel;
 
 public class MafiaGameView extends BaseActivity {
     private MafiaRoomsModel mafiaRoomsModel;
+    @BindView(R.id.playersLoading)
+    ProgressBar playersLoading;
+    @BindView(R.id.playersRecycler)
+    RecyclerView playersRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mafia_game_view);
+        ButterKnife.bind(this);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             mafiaRoomsModel = Parcels.unwrap(extras.getParcelable("mafiaRoomsModel"));
+            getSupportActionBar().setTitle(mafiaRoomsModel.getRoomName());
         }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setNightTheme() {
+        setTheme(android.R.style.ThemeOverlay_Material_Dark);
     }
 }
