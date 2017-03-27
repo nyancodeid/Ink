53095019a264731c77d2b805916bb33432a57ea3
Vladimir <vladimirarevshatyan@gmail.com>
2017-03-27 19:59:25 +0400

    back icon added

--- app/src/main/java/ink/va/activities/MafiaInfoActivity.java
+++ app/src/main/java/ink/va/activities/MafiaInfoActivity.java
@@ -1,6 +1,7 @@
 package ink.va.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ink.va.R;
 
@@ -10,5 +11,12 @@ public class MafiaInfoActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mafia_info);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return super.onOptionsItemSelected(item);
     }
 }
