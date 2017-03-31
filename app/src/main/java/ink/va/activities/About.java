c7492a6cacaed8fe2295dae48d28144271ee876b
Vladimir <vladimirarevshatyan@gmail.com>
2017-03-31 20:23:27 +0400

    minor fixes

--- app/src/main/java/ink/va/activities/About.java
+++ app/src/main/java/ink/va/activities/About.java
@@ -1,6 +1,7 @@
 package ink.va.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,11 +29,19 @@ public class About extends BaseActivity {
         ButterKnife.bind(this);
         String versionName = Version.getVersionName(this);
         versionTV.setText(getString(R.string.versionName, versionName));
-
+        getSupportActionBar().setTitle(getString(R.string.aboutTitle));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Ion.with(this).load(Constants.ANDROID_DRAWABLE_DIR + "me")
                 .withBitmap().placeholder(R.drawable.user_image_placeholder).transform(new CircleTransform()).intoImageView(developerCredit);
 
         Ion.with(this).load(Constants.ANDROID_DRAWABLE_DIR + "jean")
                 .withBitmap().placeholder(R.drawable.user_image_placeholder).transform(new CircleTransform()).intoImageView(uxCredit);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return super.onOptionsItemSelected(item);
+
     }
 }
